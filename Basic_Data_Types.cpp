8747ed303fbb5e72fa1a68261b1c9ee74cf0b86d
urvashi-code1255 <urvashianand0906@gmail.com>
2020-06-16 21:31:10 +0530

    added

---
+++ Basic_Data_Types.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    // Complete the code.
+    int a;
+    long b;
+    char c;
+    float f;
+    double d;
+    cin>>a>>b>>c>>f>>d;
+    printf("%d\n%ld\n%c\n%f\n%lf\n",a,b,c,f,d);
+    return 0;
+}
+
